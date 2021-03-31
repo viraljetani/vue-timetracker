1921a9be6951d1c9791bd8d5936ee58e34d41287
Viral Jetani <viral.jetani@gmail.com>
2021-04-01 00:09:05 +1100

    installed tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/* Using the config from https://www.tailwind-kit.com/started to use its components */
+module.exports = {
+  important: true,
+  // Active dark mode on class basis
+  darkMode: "class",
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  purge: {
+    content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+    // These options are passed through directly to PurgeCSS
+  },
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        check: "url('/icons/check.svg')",
+        landscape: "url('/images/landscape/2.jpg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
+};
